4f13e67a8c6ce3ce16764d952aac90608d7536f2
Caleb Logemann <clogema@sandia.gov>
2026-02-20 16:15:13 -0700

    Added python tools to sod shock tube test

--- end_to_end_tests/sod_shock_tube/sod_shock_tube.py
+++ end_to_end_tests/sod_shock_tube/sod_shock_tube.py
@@ -1,17 +1,43 @@
-from scipy.constants import electron_mass, elementary_charge
+import euler
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import read_mesh_data
+from scipy.constants import electron_mass
+import species
 import subprocess
+
+electron_species = species.Species(mass = electron_mass)
+
+mass_density_left = 1.0
+pressure_left = 1.0
+bulk_velocity_left = np.array([0., 0., 0.])
+
+mass_density_right = 0.125
+pressure_right = 0.1
+bulk_velocity_right = np.array([0., 0., 0.])
+
+number_density_left = mass_density_left / electron_species.mass
+temperature_left = euler.temperature(number_density_left, pressure_left)
+
+number_density_right = mass_density_right / electron_species.mass
+temperature_right = euler.temperature(number_density_right, pressure_right)
+
+sound_speed_left = euler.speed_of_sound(electron_species, mass_density_left, pressure_left)
+max_wavespeed_left = np.max(np.abs(bulk_velocity_left)) + sound_speed_left
+
+sound_speed_right = euler.speed_of_sound(electron_species, mass_density_right, pressure_right)
+max_wavespeed_right = np.max(np.abs(bulk_velocity_right)) + sound_speed_right
+
+max_wavespeed = max(max_wavespeed_left, max_wavespeed_right)
 
 domain_length = 1.0
 num_elements = 400
 dx = domain_length / num_elements
 cfl = 0.5
-max_wavespeed = 1.
 dt = cfl * dx / max_wavespeed
 initial_final_time = 0.5 * domain_length / max_wavespeed
-# num_time_steps = int(initial_final_time / dt)
-# final_time = num_time_steps * dt
-
-num_time_steps = 1
+num_time_steps = int(initial_final_time / dt)
 final_time = num_time_steps * dt
 
 input_deck_contents = f"""
@@ -23,7 +49,6 @@ Mesh:
   Type: line
   Lengths: [{domain_length}]
   Number of Elements: [{num_elements}]
-  Periodic Dimensions: [x]
 
 Time Stepping:
   Number of Time Steps: {num_time_steps}
@@ -38,9 +63,16 @@ Euler Fluids:
   Basis Order: 0
   Initial Conditions:
     - Species: [neutral_electron]
-      Constant:
-        Number Density: 1e16
-        Temperature: 300
+      Sod:
+        Discontinuity Location: {0.5 * domain_length}
+        Left State:
+          Number Density: {number_density_left}
+          Temperature: {temperature_left}
+          Bulk Velocity: [{bulk_velocity_left[0]}, {bulk_velocity_left[1]}, {bulk_velocity_left[2]}]
+        Right State:
+          Number Density: {number_density_right}
+          Temperature: {temperature_right}
+          Bulk Velocity: [{bulk_velocity_right[0]}, {bulk_velocity_right[1]}, {bulk_velocity_right[2]}]
 
 Particles:
   Boundary Conditions: []
@@ -53,15 +85,31 @@ Output:
 """
 
 def run(mfpic_executable):
-    yaml = "sod_shock_tube.yaml"
-    with open(yaml, 'w') as input_deck:
-        input_deck.write(input_deck_contents)
+  yaml = "sod_shock_tube.yaml"
+  with open(yaml, 'w') as input_deck:
+    input_deck.write(input_deck_contents)
 
-    result = subprocess.run([mfpic_executable, "-i", yaml])
-    result.check_returncode()
+  result = subprocess.run([mfpic_executable, "-i", yaml])
+  result.check_returncode()
 
 def analyze():
-    pass
+  timesteps, mesh_data = read_mesh_data.read_mesh_data()
+
+  points = mesh_data[0]['points']
+  x_points = points[:, 0]
+
+  os.makedirs("Figures", exist_ok=True)
+  for i in range(len(timesteps)):
+    mass_density = mesh_data[i]['species_0'][:, 0]
+    time = timesteps[i]
+
+    fig, axes = plt.subplots()
+    axes.plot(x_points, mass_density)
+    axes.set_title(f"Mass Density At Time = {time}")
+    axes.set_xlabel('x')
+    axes.set_ylabel('rho')
+    fig.savefig(f"Figures/MassDensity{i:03}.png")
+    plt.close(fig)
 
 if __name__ == "__main__":
   import sys
