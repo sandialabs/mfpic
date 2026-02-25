380778b0e7f583da2895ee61fbf614642aeaa19a
Logan Tate Meredith <lmeredi@sandia.gov>
2026-02-25 16:49:07 -0700

    Remove boilerplate from package.py

--- spack_repo/mfpic/packages/mfpic/package.py
+++ spack_repo/mfpic/packages/mfpic/package.py
@@ -2,41 +2,23 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install mfpic
-#
-# You can edit this file again by typing:
-#
-#     spack edit mfpic
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
-
 from spack_repo.builtin.build_systems.cmake import CMakePackage, generator
 from spack.package import *
 
-
 class Mfpic(CMakePackage):
-    """FIXME: Put a proper description of your package here."""
+    """Multi-Fidelity Particle-In-Cell."""
 
     homepage = "https://github.com/sandialabs/mfpic"
-    git = "git@github.com:sandialabs/mfpic.git"
+    git = "https://github.com/sandialabs/mfpic.git"
 
-    # FIXME: Add a list of GitHub accounts to
-    # notify when the package is updated.
-    # maintainers("github_user1", "github_user2")
+    maintainers(
+        "reuterb"
+        "logantm2",
+        "caleblogemann",
+        "gyalla",
+    )
 
-    # FIXME: Add the SPDX identifier of the project's license below.
-    # See https://spdx.org/licenses/ for a list. Upon manually verifying
-    # the license, set checked_by to your Github username.
-    license("UNKNOWN", checked_by="github_user1")
+    license("BSD-3-Clause", checked_by="logantm2")
 
     version("main", branch="main")
 
