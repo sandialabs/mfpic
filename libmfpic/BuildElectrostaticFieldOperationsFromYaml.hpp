66231b12a16bbded4cef564f5efbcaf4cefa0f36
Logan Tate Meredith <lmeredi@sandia.gov>
2026-02-26 15:19:57 -0700

    Doxygenate buildElectrostaticFieldOperations

--- libmfpic/BuildElectrostaticFieldOperationsFromYaml.hpp
+++ libmfpic/BuildElectrostaticFieldOperationsFromYaml.hpp
@@ -12,6 +12,15 @@ class DirichletBoundaryConditions;
 class Discretization;
 class ElectrostaticFieldOperations;
 
+/**
+  @brief Construct an electrostatic field operations object using a specification from a YAML file.
+
+  @param[in] fields_node                   A YAML node containing the fields specifications.
+  @param[in] electrostatic_discretization  Discretization used by the field solver.
+  @param[in] dirichlet_boundary_conditions Dirichlet boundary conditions.
+
+  @returns A smart pointer to electrostatic field operations.
+ */
 std::unique_ptr<ElectrostaticFieldOperations> buildElectrostaticFieldOperations(
   const YAML::Node& fields_node,
   Discretization& electrostatic_discretization,
