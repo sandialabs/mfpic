6c1da44ce5d0cf4b0fd9100747d19cb0d8a32d30
Logan Tate Meredith <lmeredi@sandia.gov>
2026-02-26 17:19:58 -0700

    Check a nontrivial case with zero charge density

--- libmfpic/ElectrostaticFieldOperationsWithBoltzmannElectrons.test.cpp
+++ libmfpic/ElectrostaticFieldOperationsWithBoltzmannElectrons.test.cpp
@@ -69,3 +69,48 @@ TEST(ElectrostaticFieldOperationsWithBoltzmannElectrons, BoltzmannElectronsSolve
   const mfem::GridFunction potential_with_boltzmann_electrons = state_with_boltzmann_electrons.getPotential();
   EXPECT_DOUBLE_EQ(potential_without_boltzmann_electrons.DistanceTo(potential_with_boltzmann_electrons), 0.0);
 }
+
+// Mathematica
+//
+// solution :=
+//  NDSolve[{-u''[x] + Exp[u[x]] == 0, u[0] == 0, u[1] == 0}, u[x], x]
+// nodes := Subdivide[10]
+// (u[x] /. solution)[[1]] /. x -> nodes
+
+// {0., -0.0414356, -0.0732684, -0.0957998, -0.109238, -0.113704,
+// -0.109238, -0.0957998, -0.0732684, -0.0414356, 3.27626*10^-9}
+TEST(ElectrostaticFieldOperationsWithBoltzmannElectrons, ZeroChargeWithDirichletBoundaries) {
+  constexpr int num_elems = 10;
+  mfem::Mesh mesh = mfem::Mesh::MakeCartesian1D(num_elems);
+  constexpr int hgrad_order = 1;
+  Discretization es_discretization(&mesh, hgrad_order);
+  std::unordered_map<std::string, int> side_name_to_boundary_attribute = getSideNameToBoundaryAttributeForInlineMeshes(
+    mesh.Dimension());
+  const int left_boundary_attribute = side_name_to_boundary_attribute["left"];
+  const int right_boundary_attribute = side_name_to_boundary_attribute["right"];
+  std::unordered_map<int, double> boundary_attribute_to_dirichlet_value{
+    {left_boundary_attribute, 0.},
+    {right_boundary_attribute, 0.}};
+  auto dirichlet_bcs = std::make_unique<DirichletBoundaryConditionsConstant>(
+    boundary_attribute_to_dirichlet_value, es_discretization);
+  constexpr double reference_number_density = constants::permittivity / constants::elementary_charge;
+  constexpr double temperature = constants::elementary_charge / constants::boltzmann_constant;
+  ElectrostaticFieldOperationsWithBoltzmannElectrons es_field_operations(
+    es_discretization,
+    std::move(dirichlet_bcs),
+    reference_number_density,
+    temperature
+  );
+  ElectrostaticFieldState es_field_state(es_discretization);
+
+  IntegratedCharge integrated_charge(es_discretization);
+  integrated_charge.setIntegratedChargeValue(0.);
+
+  es_field_operations.fieldSolve(es_field_state, integrated_charge);
+
+  const mfem::GridFunction& potential = es_field_state.getPotential();
+  const mfem::Vector expected_potential({0., -0.0414356, -0.0732684, -0.0957998, -0.109238, -0.113704,
+    -0.109238, -0.0957998, -0.0732684, -0.0414356, 0.0});
+  constexpr double relative_tolerance = 1.0e-3;
+  ASSERT_NEAR(potential.DistanceTo(expected_potential) / expected_potential.Norml2(), 0.0, relative_tolerance);
+}
