d677b39660e119988daf03e7e1582be189915d75
Logan Tate Meredith <lmeredi@sandia.gov>
2026-02-27 09:26:36 -0700

    Modify for Boltzmann electrons

--- end_to_end_tests/sheath/sheath.py
+++ end_to_end_tests/sheath/sheath.py
@@ -8,17 +8,19 @@ dx = debye_length
 num_elements = 120
 domain_length = num_elements * debye_length
 most_probable_ion_speed = np.sqrt(2.0 * Boltzmann * temperature / proton_mass)
-most_probable_electron_speed =np.sqrt(2.0 * Boltzmann * temperature / electron_mass)
 ion_acoustic_transit_time = domain_length / most_probable_ion_speed
-electron_plasma_frequency = np.sqrt(number_density * elementary_charge**2.0 / electron_mass / epsilon_0)
-dt = min(dx / (3.0 * most_probable_electron_speed), 0.1 / electron_plasma_frequency)
+ion_plasma_frequency = np.sqrt(number_density * elementary_charge**2.0 / proton_mass / epsilon_0)
+dt = min(dx / (3.0 * most_probable_ion_speed), 0.1 / ion_plasma_frequency)
 num_macroparticles_per_population = 1000 * num_elements
 bohm_speed = np.sqrt(Boltzmann * temperature / proton_mass)
 bohm_current_density = number_density * elementary_charge * bohm_speed * np.exp(-0.5)
-ion_wall_flux_density = 2 * bohm_current_density / elementary_charge
-source_number_density = ion_wall_flux_density / domain_length * dt
-source_number_density_fraction = source_number_density / number_density
-source_num_macroparticles = int(source_number_density_fraction * num_macroparticles_per_population)
+ion_wall_flux = 2 * bohm_current_density / elementary_charge
+source_number_density = ion_wall_flux / domain_length * dt
+source_num_macroparticles = int(source_number_density / number_density * num_macroparticles_per_population)
+electron_thermal_speed = np.sqrt(8.0 * Boltzmann * temperature / np.pi / electron_mass)
+electron_boundary_flux_per_unit_number_density = 2.0 * electron_thermal_speed / 4.0
+electron_source_rate = ion_wall_flux
+electron_reference_number_density = electron_source_rate / electron_boundary_flux_per_unit_number_density
 
 def run(mfpic_executable):
   import subprocess
@@ -26,6 +28,9 @@ def run(mfpic_executable):
   input_deck_contents = f"""
 Fields:
   Basis Order: 1
+  Boltzmann Electrons:
+    Reference Number Density: {electron_reference_number_density}
+    Temperature: {temperature}
   Boundary Conditions:
     - Side: left
       Value: 0.0
@@ -42,9 +47,6 @@ Time Stepping:
   Time Step Size: {dt}
 
 Species:
-  electron:
-    Mass: {electron_mass}
-    Charge: {-elementary_charge}
   proton:
     Mass: {proton_mass}
     Charge: {elementary_charge}
@@ -53,17 +55,17 @@ Particles:
   Boundary Conditions: []
   Default Boundary Condition: Absorbing
   Initial Conditions:
-    - Species: [proton, electron]
+    - Species: [proton]
       Number of Macroparticles per Species: {num_macroparticles_per_population}
       Constant:
         Temperature: {temperature}
         Number Density: {number_density}
   Sources:
-    - Species: [proton, electron]
+    - Species: [proton]
       Number of Macroparticles per Species: {source_num_macroparticles}
       Constant:
         Temperature: {temperature}
-        Number Density: {number_density * source_number_density_fraction}
+        Number Density: {source_number_density}
 
   """
 
