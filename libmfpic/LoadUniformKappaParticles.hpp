9a20376d84c0cb355bafd3f85ad302b700f30952
Logan Tate Meredith <lmeredi@sandia.gov>
2026-04-06 16:49:41 -0600

    Encapsulate kappa velocity generation (#27)
    * Add kappa velocity generator
    
    * Use it
    
    * Copy relevant unit tests from LoadUniformKappaParticles
    
    * Use nu as previously defined
    
    Co-authored-by: reuterb <bwreute@sandia.gov>
    
    ---------
    
    Co-authored-by: reuterb <bwreute@sandia.gov>

--- libmfpic/LoadUniformKappaParticles.hpp
+++ libmfpic/LoadUniformKappaParticles.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-#include <libmfpic/Constants.hpp>
+#include <libmfpic/GenerateKappaVelocity.hpp>
 #include <libmfpic/MeshUtilities.hpp>
 #include <libmfpic/ParticleContainer.hpp>
 #include <libmfpic/Species.hpp>
@@ -39,26 +39,14 @@ ParticleContainer loadUniformKappaParticles(
   Generator& generator,
   std::shared_ptr<mfem::Mesh> mesh
 ) {
-  assert(bulk_velocity.Size() == 3);
-  assert(temperature >= 0.0);
-
   ParticleContainer particles;
 
   const double mesh_volume = getMeshVolume(*mesh);
   const double particle_weight = number_density * mesh_volume / num_particles;
   UniformMeshDistribution position_distribution(mesh);
 
-  const double v_thermal_squared = constants::boltzmann_constant * temperature / species.mass;
   for (int i = 0; i < num_particles; ++i) {
-    mfem::Vector velocity = bulk_velocity;
-    if (v_thermal_squared > 0.0) {
-      const double nu = 2.0 * kappa + 1.0;
-      const double scale = std::sqrt(kappa * 2.0 * v_thermal_squared / (2.0 * kappa + 1.0));
-      std::student_t_distribution<double> velocity_distribution(nu);
-      for (int dimension = 0; dimension < 3; dimension++) {
-        velocity[dimension] += scale * velocity_distribution(generator); 
-      }
-    }
+    const mfem::Vector velocity = generateKappaVelocity(bulk_velocity, temperature, kappa, species.mass, generator);
 
     mfem::Vector position({0.0, 0.0, 0.0});
     const auto [random_mesh_position, element] = position_distribution.generateRandomPointAndElement(generator);
