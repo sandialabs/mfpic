f7ac7662181ed06d83f9a8cf217284efb739066c
Gopal Yalla <gyalla@sandia.gov>
2026-04-17 09:02:29 -0600

    Randomizing particle generation

--- libmfpic/RunSimulation.cpp
+++ libmfpic/RunSimulation.cpp
@@ -77,7 +77,8 @@ void runSimulation(int argc, char* argv[]) {
     num_species
   );
 
-  std::default_random_engine generator;
+  //std::default_random_engine generator;
+  std::random_device rd; std::default_random_engine generator(rd());
   ParticleContainer particle_container = buildParticlesFromYaml(
     main["Particles"]["Initial Conditions"],
     species_map,
