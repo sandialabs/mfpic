93c90ff7c12f8e2c55c4d1c138c9f2e44fc95eb1
Gopal Yalla <gyalla@sandia.gov>
2026-04-21 10:46:42 -0600

    Initial implementation of dump variance reduced particle moments

--- libmfpic/DumpParticles.cpp
+++ libmfpic/DumpParticles.cpp
@@ -159,4 +159,60 @@ void dumpParticleMoments(
   }
 }
 
+void dumpVarianceReducedParticleMoments(
+  ParticleOperations& particle_operations,
+  const ParticleContainer& particles,
+  const LowFidelityState& low_fidelity_state,
+  const DGEulerOperations& low_fidelity_operations,
+  const std::string& file_prefix,
+  const int step,
+  const double time) 
+{
+
+  mfem::DenseMatrix variance_reduced_number_density = particle_operations.getVarianceReducedNumberDensity(particles,low_fidelity_state,low_fidelity_operations);
+  mfem::DenseTensor variance_reduced_bulk_velocity = particle_operations.getVarianceReducedBulkVelocity(particles,low_fidelity_state,low_fidelity_operations);
+  mfem::DenseMatrix variance_reduced_temperature = particle_operations.getVarianceReducedTemperature(particles,low_fidelity_state,low_fidelity_operations);
+
+  mfem::Mesh& mesh = particle_operations.getMesh();
+  const int nelem = mesh.GetNE();
+
+  for (int s = 0; s < particle_operations.getNumSpecies(); ++s) {
+    std::string filename = file_prefix + "_species_" + std::to_string(s) + ".csv";
+
+    const bool need_header = fileIsEmpty(filename);
+
+    std::ofstream out;
+    if (step > 0)
+      out.open(filename, std::ios::out | std::ios::app);
+    else
+      out.open(filename, std::ios::out | std::ios::trunc);  
+    if (!out) throw std::runtime_error("Failed to open CSV file: " + filename);
+
+    out.setf(std::ios::scientific);
+    out << std::setprecision(17);
+
+    if (need_header) {
+      out << "step,time,elem,x,y,z,number_density,temperature,bulk_velocity_0,bulk_velocity_1,bulk_velocity_2\n";
+    }
+
+    for (int e = 0; e < nelem; ++e) {
+      mfem::Vector element_point(3);
+      element_point = 0.0;
+      const int dim = mesh.SpaceDimension();
+      mfem::Vector element_point_view(element_point.GetData(), dim);   
+      mesh.GetElementCenter(e, element_point_view);
+
+      const mfem::Vector bulk_velocity_in_element(variance_reduced_bulk_velocity(s).GetColumn(e), 3);
+
+      out << step << ","
+          << time << ","
+          << e << ","
+          << element_point(0) << "," << element_point(1) << "," << element_point(2) << ","
+          << variance_reduced_number_density(e, s) << ","
+          << variance_reduced_number_density(e, s) << ","
+          << bulk_velocity_in_element(0) << "," << bulk_velocity_in_element(1) << "," << bulk_velocity_in_element(2) << "\n";
+    }
+  }
+}
+
 } // namespace mfpic
--- libmfpic/DumpParticles.hpp
+++ libmfpic/DumpParticles.hpp
@@ -6,9 +6,20 @@ namespace mfpic {
 
 class ParticleContainer;
 class ParticleOperations;
+class DGEulerOperations;
+class LowFidelityState;
 
 void dumpParticles(const ParticleContainer& particles, double simulation_time, const std::string filename = "particles.h5part");
 
 void dumpParticleMoments(ParticleOperations& particle_operations,const ParticleContainer& particles,const std::string& file_prefix,const int step,const double time); 
+
+void dumpVarianceReducedParticleMoments(
+    ParticleOperations& particle_operations,
+    const ParticleContainer& particles,
+    const LowFidelityState& low_fidelity_state,
+    const DGEulerOperations& low_fidelity_operations,
+    const std::string& file_prefix,
+    const int step,
+    const double time); 
 
 } // namespace mfpic
