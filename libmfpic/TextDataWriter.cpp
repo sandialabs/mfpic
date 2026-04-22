c1584647faf36af5ff5fa5575daaf7b1ea874d59
Caleb Logemann <clogema@sandia.gov>
2026-04-22 09:34:34 -0600

    Added Total_Fluid_Kinetic_Energy to text output

--- libmfpic/TextDataWriter.cpp
+++ libmfpic/TextDataWriter.cpp
@@ -11,7 +11,7 @@ namespace {
 
 void writeHeaderToCSVFile(std::ofstream& csv_file) {
   csv_file << std::setprecision(std::numeric_limits<double>::max_digits10);
-  csv_file << "# Time_Step Time Field_Energy Total_Fluid_Energy" << std::endl;
+  csv_file << "# Time_Step Time Field_Energy Total_Fluid_Energy Total_Fluid_Kinetic_Energy" << std::endl;
 }
 
 void writeLineToCSVFile(
@@ -19,9 +19,15 @@ void writeLineToCSVFile(
   const int i_time_step,
   const double time,
   const double field_energy,
-  const double total_fluid_energy)
+  const double total_fluid_energy,
+  const double total_fluid_kinetic_energy)
 {
-  csv_file << i_time_step << " " << time << " " << field_energy << " " << total_fluid_energy << std::endl;
+  csv_file <<
+  i_time_step << " " <<
+  time << " " <<
+  field_energy << " " <<
+  total_fluid_energy << " " <<
+  total_fluid_kinetic_energy << std::endl;
 }
 
 }
@@ -39,12 +45,12 @@ void TextDataWriter::output(
   const std::vector<ElectrostaticFieldState>& low_fidelity_field_states,
   const ElectrostaticFieldOperations& electrostatic_field_operations,
   const std::vector<LowFidelityState>& low_fidelity_states,
-  const std::vector<std::unique_ptr<LowFidelityOperations>>& low_fidelity_operations,
+  const std::vector<std::unique_ptr<LowFidelityOperations>>& low_fidelity_operations_vector,
   const int i_time_step,
   const double time)
 {
   const double particle_field_energy = electrostatic_field_operations.fieldEnergy(particle_electrostatic_field_state);
-  writeLineToCSVFile(main_csv_file_, i_time_step, time, particle_field_energy, 0.);
+  writeLineToCSVFile(main_csv_file_, i_time_step, time, particle_field_energy, 0., 0.);
 
   for (int i_low_fidelity_model = 0; i_low_fidelity_model < std::ssize(low_fidelity_csv_files_); ++i_low_fidelity_model) {
     std::ofstream& low_fidelity_csv_file = low_fidelity_csv_files_[i_low_fidelity_model];
@@ -53,9 +59,17 @@ void TextDataWriter::output(
     const double low_fidelity_field_energy = electrostatic_field_operations.fieldEnergy(low_fidelity_field_state);
 
     const LowFidelityState& low_fidelity_state = low_fidelity_states[i_low_fidelity_model];
-    const double total_fluid_energy = low_fidelity_operations[i_low_fidelity_model]->computeTotalEnergy(low_fidelity_state);
+    const LowFidelityOperations& low_fidelity_operations = *low_fidelity_operations_vector[i_low_fidelity_model];
+    const double total_fluid_energy = low_fidelity_operations.computeTotalEnergy(low_fidelity_state);
+    const double total_fluid_kinetic_energy = low_fidelity_operations.computeTotalKineticEnergy(low_fidelity_state);
 
-    writeLineToCSVFile(low_fidelity_csv_file, i_time_step, time, low_fidelity_field_energy, total_fluid_energy);
+    writeLineToCSVFile(
+      low_fidelity_csv_file,
+      i_time_step,
+      time,
+      low_fidelity_field_energy,
+      total_fluid_energy,
+      total_fluid_kinetic_energy);
   }
 }
 }
