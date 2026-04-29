0db52abbb24e974150c6df5696b4e3bb26658d66
Logan Tate Meredith <lmeredi@sandia.gov>
2026-04-29 12:37:45 -0600

    Start unit tests

--- libmfpic/ParticleContainer.test.cpp
+++ libmfpic/ParticleContainer.test.cpp
@@ -41,4 +41,38 @@ TEST(ParticleContainer, AddParticlesAddsParticles) {
   EXPECT_EQ(added_particle.element, element);
 }
 
+void checkThatParticlesAreSorted(const ParticleContainer& particles) {
+  for (
+    auto particle_iter = particles.begin();
+    std::distance(particle_iter, particles.end()) > 1;
+    std::advance(particle_iter, 1)
+  ) {
+    const Particle& this_particle = *particle_iter;
+    const Particle& next_particle = *std::next(particle_iter);
+    if (this_particle.element == next_particle.element) {
+      ASSERT_LE(
+        particles.getParticleSpeciesIndex(this_particle),
+        particles.getParticleSpeciesIndex(next_particle)
+      );
+    }
+    else {
+      ASSERT_LT(this_particle.element, next_particle.element);
+    }
+  }
+}
+
+TEST(ParticleContainer, SortingAlreadySortedParticlesPreservesSortedness) {
+  const Species species_0{.name = "species_0"};
+  const Species species_1{.name = "species_1"};
+  ParticleContainer already_sorted;
+  already_sorted.addParticle(Particle{.element = 0, .species = species_0});
+  already_sorted.addParticle(Particle{.element = 0, .species = species_1});
+  already_sorted.addParticle(Particle{.element = 1, .species = species_0});
+  already_sorted.addParticle(Particle{.element = 1, .species = species_1});
+
+  already_sorted.sortByElementThenSpecies();
+
+  checkThatParticlesAreSorted(already_sorted);
+}
+
 } // namespace
