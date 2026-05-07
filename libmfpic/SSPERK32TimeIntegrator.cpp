dd4f20fbf346ad14e2c872f4507b14a258ea4ade
Caleb Logemann <clogema@sandia.gov>
2026-05-07 14:03:55 -0600

    Added SSPERK32TimeIntegrator

---
+++ libmfpic/SSPERK32TimeIntegrator.cpp
@@ -0,0 +1,60 @@
+#include <libmfpic/ForwardEulerTimeIntegrator.hpp>
+
+#include <libmfpic/ElectrostaticFieldOperations.hpp>
+#include <libmfpic/IntegratedCharge.hpp>
+#include <libmfpic/LowFidelityOperations.hpp>
+
+namespace mfpic {
+
+void ForwardEulerTimeIntegrator::advanceTimestep(
+  std::vector<LowFidelityState>& low_fidelity_states,
+  std::vector<ElectrostaticFieldState>& low_fidelity_field_states,
+  const std::vector<std::unique_ptr<LowFidelityOperations>>& low_fidelity_operations,
+  ParticleContainer& particle_container,
+  const ParticleOperations& /*particle_operations*/,
+  ElectrostaticFieldState& /*field_state*/,
+  ElectrostaticFieldOperations& field_operations,
+  double dt) const
+{
+  if (particle_container.numParticles() > 0) {
+    errorWithUserMessage("SSPERK32 can only be used with no PIC particles.");
+  }
+
+  for (int i_lf_model = 0; i_lf_model < std::ssize(low_fidelity_operations); i_lf_model++) {
+    const LowFidelityOperations& operations = *low_fidelity_operations[i_lf_model];
+    LowFidelityState& low_fidelity_state = low_fidelity_states[i_lf_model];
+    const ElectrostaticFieldState& low_fidelity_field_state = low_fidelity_field_states[i_lf_model];
+
+    LowFidelityState rhs_0 = operations.computeRHS(low_fidelity_state, low_fidelity_field_state);
+    LowFidelityState low_fidelity_state_1(low_fidelity_state);
+    low_fidelity_state_1.addScaledState((5. / 6.) * dt, rhs_0);
+
+    IntegratedCharge low_fidelity_charge_1(discretization_);
+    low_fidelity_charge_1.addCharge(operations.assembleCharge(low_fidelity_state_1));
+    ElectrostaticFieldState low_fidelity_field_state_1 = low_fidelity_field_states[i_lf_model];
+    field_operations.fieldSolve(low_fidelity_field_state_1, low_fidelity_charge_1);
+
+    LowFidelityState rhs_1 = operations.computeRHS(low_fidelity_state_1, low_fidelity_field_state_1);
+
+    LowFidelityState low_fidelity_state_2(low_fidelity_state);
+    low_fidelity_state_2.addScaledState((11. / 24.) * dt, rhs_0);
+    low_fidelity_state_2.addScaledState((11. / 24.) * dt, rhs_1);
+
+    IntegratedCharge low_fidelity_charge_2(discretization_);
+    low_fidelity_charge_2.addCharge(operations.assembleCharge(low_fidelity_state_2));
+    ElectrostaticFieldState low_fidelity_field_state_2 = low_fidelity_field_states[i_lf_model];
+    field_operations.fieldSolve(low_fidelity_field_state_2, low_fidelity_charge_2);
+
+    LowFidelityState rhs_2 = operations.computeRHS(low_fidelity_state_2, low_fidelity_field_state_2);
+
+    low_fidelity_state.addScaledState((24. / 55.) * dt, rhs_0);
+    low_fidelity_state.addScaledState((1. / 5.) * dt, rhs_1);
+    low_fidelity_state.addScaledState((4. / 11.) * dt, rhs_2);
+
+    IntegratedCharge low_fidelity_charge(discretization_);
+    low_fidelity_charge.addCharge(operations.assembleCharge(low_fidelity_state));
+    field_operations.fieldSolve(low_fidelity_field_states[i_lf_model], low_fidelity_charge);
+  }
+}
+
+}
