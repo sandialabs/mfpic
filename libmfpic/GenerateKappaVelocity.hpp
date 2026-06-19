f0b21af41bd7b8d5317c63c4ec4513561099a1a4
Logan Tate Meredith <lmeredi@sandia.gov>
2026-06-18 18:27:48 -0600

    Put defs in source file

--- libmfpic/GenerateKappaVelocity.hpp
+++ libmfpic/GenerateKappaVelocity.hpp
@@ -24,24 +24,7 @@ mfem::Vector generateKappaVelocity(
   double kappa,
   double mass,
   RandomNumberGenerator& generator
-) {
-  assert(bulk_velocity.Size() == 3);
-  assert(temperature >= 0.0);
-  assert(mass > 0.0);
-
-  const double v_thermal_squared = constants::boltzmann_constant * temperature / mass;
-  mfem::Vector velocity = bulk_velocity;
-  if (v_thermal_squared > 0.0) {
-    const double nu = 2.0 * kappa + 1.0;
-    const double scale = std::sqrt(kappa * 2.0 * v_thermal_squared / nu);
-    std::student_t_distribution<double> velocity_distribution(nu);
-    for (int dimension = 0; dimension < 3; dimension++) {
-      velocity[dimension] += scale * velocity_distribution(generator);
-    }
-  }
-
-  return velocity;
-}
+);
 
 /**
  * @brief Generate a three-dimensional velocity vector from an isotropic Kappa distribution.
@@ -59,36 +42,6 @@ mfem::Vector generateIsotropicKappaVelocity(
   double temperature,
   double kappa,
   double mass,
-  RandomNumberGenerator& generator)
-{
-  assert(bulk_velocity.Size() == 3);
-  assert(temperature >= 0.0);
-  assert(mass > 0.0);
-  assert(kappa > 1.5);
-
-  mfem::Vector velocity = bulk_velocity;
-
-  if (temperature == 0.0) {
-    return velocity;
-  }
-
-  const double nu = 2.0 * kappa - 1.0;
-  const double w_squared =
-  ((nu - 2.0) / nu) *
-  constants::boltzmann_constant *
-  temperature / mass;
-  const double w = std::sqrt(w_squared);
-  std::normal_distribution<double> normal_distribution(0.0, 1.0);
-  std::gamma_distribution<double> gamma_distribution(
-    0.5 * nu,
-    2.0);
-  const double chi_squared = gamma_distribution(generator);
-  const double scale = std::sqrt(nu / chi_squared);
-  for (int dimension = 0; dimension < 3; ++dimension) {
-    velocity(dimension) +=
-      w * scale * normal_distribution(generator);
-  }
-  return velocity;
-}
+  RandomNumberGenerator& generator);
 
 } // namespace mfpic
