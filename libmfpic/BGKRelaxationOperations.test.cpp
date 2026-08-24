6b11d4e1c169a63a38636338066f476ede389e5d
Logan Tate Meredith <lmeredi@sandia.gov>
2026-08-24 14:23:10 -0600

    Set up unit tests

---
+++ libmfpic/BGKRelaxationOperations.test.cpp
@@ -0,0 +1,13 @@
+#include <libmfpic/BGKRelaxationOperations.hpp>
+
+#include <gtest/gtest.h>
+
+namespace {
+
+using namespace mfpic;
+
+TEST(BGKRelaxationOperations, JustPass) {
+  ASSERT_TRUE(true);
+}
+
+} // namespace
