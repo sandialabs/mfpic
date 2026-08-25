1df89aab0998d754e9115834f0dd8379fece6ec5
Logan Tate Meredith <lmeredi@sandia.gov>
2026-08-25 10:34:48 -0600

    Check the relaxation fraction is as expected

--- libmfpic/BGKRelaxationOperations.test.cpp
+++ libmfpic/BGKRelaxationOperations.test.cpp
@@ -84,4 +84,44 @@ TEST(BGKRelaxationOperations, EveryParticleRelaxesWhenTimestepIsLargeComparedToM
   }
 }
 
+TEST(BGKRelaxationOperations, FractionOfRelaxedParticlesIsAsExpected) {
+  Discretization discretization(&mesh, order);
+  ParticleOperations particle_operations(
+    discretization,
+    empty_particle_boundary_factory_list,
+    default_reflecting_particle_boundary_factory,
+    one_species
+  );
+  constexpr int num_particles = 1000;
+  ParticleContainer unrelaxed_particles;
+  for (int iparticle = 0; iparticle < num_particles; iparticle++) {
+    unrelaxed_particles.addParticle(Particle{.velocity=mfem::Vector{iparticle, 2*iparticle, 300*iparticle}, .element=0, .species=default_species, .weight=1.0});
+  }
+
+  ParticleContainer relaxed_particles = unrelaxed_particles;
+  constexpr double collision_frequency = 1.0;
+  BGKRelaxationOperations relaxer(collision_frequency, default_species);
+  constexpr double dt = 1.0 / collision_frequency;
+  RandomNumberGenerator generator;
+  relaxer.performCollisions(dt, generator, relaxed_particles, particle_operations);
+
+  ASSERT_EQ(unrelaxed_particles.numParticles(), relaxed_particles.numParticles());
+  int num_relaxed_particles = 0;
+  for (const auto [unrelaxed_particle, relaxed_particle] : std::views::zip(unrelaxed_particles, relaxed_particles)) {
+    const mfem::Vector unrelaxed_velocity = unrelaxed_particle.velocity;
+    const mfem::Vector relaxed_velocity = relaxed_particle.velocity;
+    bool particle_is_untouched = true;
+    for (int idim = 0; idim < 3; idim++) {
+      particle_is_untouched = particle_is_untouched and unrelaxed_velocity[idim] == relaxed_velocity[idim];
+    }
+    if (not particle_is_untouched) {
+      num_relaxed_particles += 1;
+    }
+  }
+  const double relaxation_fraction = -std::expm1(-collision_frequency * dt);
+  const double expected_num_relaxed_particles = num_particles * relaxation_fraction;
+  const double tolerance = 0.1 * expected_num_relaxed_particles;
+  EXPECT_NEAR(num_relaxed_particles, expected_num_relaxed_particles, tolerance);
+}
+
 } // namespace
