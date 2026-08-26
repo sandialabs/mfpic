d2dd1b3201d36c7f82f411a3bc5baabed0b360a6
Logan Tate Meredith <lmeredi@sandia.gov>
2026-08-26 11:12:21 -0600

    Start unit tests

---
+++ libmfpic/BuildCollisionOperationsFromYaml.test.cpp
@@ -0,0 +1,14 @@
+#include <libmfpic/BGKRelaxationOperations.hpp>
+#include <libmfpic/BuildCollisionOperationsFromYaml.hpp>
+
+#include <gtest/gtest.h>
+
+namespace {
+
+using namespace mfpic;
+
+TEST(BuildCollisionOperationsFromYaml, DoNothing) {
+  ASSERT_TRUE(true);
+}
+
+} // namespace
