5e535db74e24f67974b90bed0ebc419cf2d69e50
Logan Tate Meredith <lmeredi@sandia.gov>
2026-08-26 11:05:57 -0600

    Doxygenate collision operations

--- libmfpic/BuildCollisionOperationsFromYaml.hpp
+++ libmfpic/BuildCollisionOperationsFromYaml.hpp
@@ -7,6 +7,14 @@
 
 namespace mfpic {
 
+/**
+ * @brief Build a set of collision operations objects from a YAML node.
+ *
+ * @param[in] collisions_node YAML node containing a sequence with collision parameters.
+ * @param[in] species_map     A map of species name to species definitions.
+ *
+ * @return An array of collision operations.
+ */
 std::vector<std::unique_ptr<CollisionOperations>> buildCollisionOperationsFromYaml(
   const YAML::Node& collisions_nodes,
   const std::unordered_map<std::string, Species>& species_map
