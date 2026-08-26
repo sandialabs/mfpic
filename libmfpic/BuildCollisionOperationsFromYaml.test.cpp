4410a37e10c52e6ef3b83d6b73f5330c5ed67fa0
Logan Tate Meredith <lmeredi@sandia.gov>
2026-08-26 11:15:38 -0600

    Check that empty yaml does not return collisions

--- libmfpic/BuildCollisionOperationsFromYaml.test.cpp
+++ libmfpic/BuildCollisionOperationsFromYaml.test.cpp
@@ -7,8 +7,17 @@ namespace {
 
 using namespace mfpic;
 
-TEST(BuildCollisionOperationsFromYaml, DoNothing) {
-  ASSERT_TRUE(true);
+const std::unordered_map<std::string, Species> species_map_with_electron = {
+  {"electron", Species{.charge = 1.6e-19, .mass = 9.11e-31}},
+};
+
+TEST(BuildCollisionOperationsFromYaml, EmptyYAMLYieldsNoCollisionOperations) {
+  std::vector<std::unique_ptr<CollisionOperations>> collision_operations = buildCollisionOperationsFromYaml(
+    YAML::Node(),
+    species_map_with_electron
+  );
+
+  ASSERT_TRUE(collision_operations.empty());
 }
 
 } // namespace
